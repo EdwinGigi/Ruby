a98a6e905453c0f31fdac5838bdfb0e8bd315d6a
Edwin Gigi <edwingigi2012@gmail.com>
2025-07-30 22:13:49 +0100

    user inputs and a addition calculator

--- App.rb
+++ App.rb
@@ -70,6 +70,14 @@ puts num.round # This will round the float -36.8 to the nearest integer, resulti
 
 puts Math.log(100) # This will output the natural logarithm of 100, which is approximately 4.605170185988091.
 
+# User Input Example
+puts "Please enter your name:"
+name = gets.chomp # This will read a line of input from the user and remove the trailing newline character.
+puts "Hello #{name}! Welcome to the Ruby application."
 
-
-
+puts "Enter a number:"
+number_1 = gets.chomp.to_i # This will read a line of input from the user, convert it to an integer, and store it in the variable number.
+puts "Enter another number to add:"
+number_2 = gets.chomp.to_i # This will read another line of input from the user, convert it to an integer, and store it in the variable number_2.
+puts "You entered the numbers: #{number_1} and #{number_2}"
+puts "The sum is: #{number_1 + number_2}"
