0f8c92a7cc43619332e650a8b404c17281d1158e
Edwin Gigi <edwingigi2012@gmail.com>
2025-07-30 21:58:27 +0100

    Create App.rb
    initial ruby commit

---
+++ App.rb
@@ -0,0 +1,18 @@
+
+# App.rb
+# This is a simple Ruby application that prints "Hello, world!" to the console.
+puts "Hello, world!"
+
+=begin
+# To run this application, save the code in a file named App.rb and execute it using the Ruby interpreter:
+=end   
+
+name = "This is my Ruby Application"
+version = 1
+is_active = true
+
+puts "Application Name: #{name}"
+puts "Version: #{version}"
+puts "Is Active: #{is_active}"
+
+
