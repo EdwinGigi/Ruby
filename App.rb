d4f09ddba24292b9b39f4a70dc0e62456d1c3a12
Edwin Gigi <edwingigi2012@gmail.com>
2025-07-31 15:08:32 +0100

    methods basics

--- App.rb
+++ App.rb
@@ -123,3 +123,17 @@ puts friends # This will output the friends array after removing the last elemen
 
 friends.push("David") # This will add "David" to the end of the friends array.
 puts friends # This will output the friends array after adding "David". 
+
+# Methods
+
+def add_number(num1, num2=99) #here num2 has a default value of 99
+    return num1 + num2
+end
+
+sum = add_number(10, 20) # This will call the add_number method with num1 as 10 and num2 as 20.
+puts sum # This will output the result of the addition, which is 30.
+
+sum = add_number(10) # This will call the add_number method with num1 as 10 and num2 as 99 (the default value).
+puts sum # This will output the result of the addition, which is 109.
+
+
