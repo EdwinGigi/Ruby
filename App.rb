ff8c2dbc36cc24a0ba4ef04c2fd2bc3a9e33a7dc
Edwin Gigi <edwingigi2012@gmail.com>
2025-07-31 15:11:42 +0100

    conditional statement basics

--- App.rb
+++ App.rb
@@ -137,3 +137,27 @@ sum = add_number(10) # This will call the add_number method with num1 as 10 and 
 puts sum # This will output the result of the addition, which is 109.
 
 
+# Conditional Statements
+
+is_student = false
+is_smart = false
+if is_student && is_smart
+    puts "You are a smart student."
+elsif is_student && !is_smart
+    puts "You are a student, but not very smart."
+elsif !is_student && is_smart
+    puts "You are not a student, but you are smart."
+else
+    puts "You are neither a student nor smart."
+end
+
+if 1 > 3
+    puts "1 is greater than 3"
+end
+
+if "a" > "b"
+    puts "string comparison was true" #a is before b in the alphabet
+end
+
+
+
